d8a6666a08c7b47e438d610e99c98ffa67850d78
arefinn <arefinn@gmail.com>
2015-12-15 00:15:40 +0530

    Models added

---
+++ ContosoUniversity/Models/Enrollment.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace ContosoUniversity.Models
+{
+    public enum Grade
+    {
+        A,B,C,D,E,F
+    }
+
+    public class Enrollment
+    {
+        public int EnrollmentId { get; set; }
+        public int CourseId { get; set; }
+        public int StudentId { get; set; }
+        public Grade? Grade { get; set; }
+        public virtual ICollection<Student> Student { get; set; }
+        public virtual ICollection<Course> Course { get; set; }
+    }
+}
